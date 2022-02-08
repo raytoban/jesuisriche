a2ba4038186456b3f68ffbbb720523afb64b51f4
ray@nab.gmail.com <johndoe@example.com>
2022-02-08 09:27:58 +0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(" Je suis riche "),
+          ),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage("images/diamond.png"),
+          ),
+        ),
+      ),
+    ),
+  );
+}
